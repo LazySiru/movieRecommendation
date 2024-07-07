cd6291cea3f3591ba58b1fe036eb3eab487e415b
LazySiru <hyo05065@berkeley.edu>
2024-07-07 11:06:19 -0700

    First refactorization has been completed. Content based model refactorization

---
+++ recommendationSystem/trigger.py
@@ -0,0 +1,15 @@
+import joblib
+import pandas as pd
+
+class trigger:
+    def __init__(self):
+        self.content_rec = joblib.load("./models/content_rec.pkl")
+        self.content_based_model = joblib.load('./models/content_based_model.pkl')
+        self.tfidf_matrix = joblib.load('./models/tfidf_matrix.pkl')
+        self.movies = pd.read_csv('./cleaned_datasets/movies.dat', 
+                        delimiter='::', 
+                        encoding = 'latin1',
+                        header = None,
+                        engine = 'python',
+                        names = ['movie_id', 'title', 'genres']
+                        )
