7d763d1a5e703a80a23575dd87442f1d3a43aa37
Sung Kim <1998sungjoong@gmail.com>
2024-10-13 14:43:14 -0400

    Pickle file for models uploaded

--- recommendationSystem/app.py
+++ recommendationSystem/app.py
@@ -3,16 +3,63 @@ import contentBasedModel as CBM
 
 app = Flask(__name__)
 
+import pickle
+import pandas as pd
+
+def get_movie_recommendations(user_id, data, k=5):
+    # Get all movie IDs
+    all_movie_ids = data['movie_id'].unique()
+    
+    # Get movie IDs that the user has already rated
+    user_rated_movies = data[data['user_id'] == user_id]['movie_id'].tolist()
+    
+    # Predict ratings for all movies that the user hasn't rated
+    movie_predictions = []
+    with open('./models/svd_model.pkl', 'rb') as f:
+        loaded_svd_model = pickle.load(f)
+
+        for movie_id in all_movie_ids:
+            if movie_id not in user_rated_movies:
+                pred = loaded_svd_model.predict(user_id, movie_id)
+                movie_predictions.append((movie_id, pred.est))
+    
+    # Sort by predicted rating and return top k recommendations
+    movie_predictions.sort(key=lambda x: x[1], reverse=True)
+    top_k_recommendations = movie_predictions[:k]
+    
+    return top_k_recommendations
+
+
 @app.route('/')
 def home():
     return "Machine Learning Model API"
 
-@app.route('/predict/contentBased', methods=['GET'])
+@app.route('/recommend/cb_model', methods=['GET'])
 def predict():
-    data = request.get_json(force=True)
-    model = CBM.contentBasedModel(data["input"][0])
+    req_data = request.get_json(force=True)
+
+    model = CBM.contentBasedModel(req_data["input"][0])
     prediction = model.get_recommendation()
+
     return jsonify({'prediction': prediction})
 
+@app.route('/recommend/svd_model', methods = ['POST'])
+def recommend_movies_svd():
+    data = pd.read_csv('./datasets/merged.csv')
+    req_data = request.get_json()
+
+    user_id = req_data['user_id']
+    res = get_movie_recommendations(user_id, data)
+
+    # extract list of movie_ids 
+    movie_ids = [movie_id for movie_id, _ in res]
+    filtered_movies = data[data['movie_id'].isin(movie_ids)]
+    return_res = filtered_movies['title'].unique().tolist()
+    return jsonify({'recommendations': return_res})
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host = 'localhost', port = 8000, debug=True)
+
+
+
--- recommendationSystem/trigger.py
+++ recommendationSystem/trigger.py
@@ -6,10 +6,11 @@ class trigger:
         self.content_rec = joblib.load("./models/content_rec.pkl")
         self.content_based_model = joblib.load('./models/content_based_model.pkl')
         self.tfidf_matrix = joblib.load('./models/tfidf_matrix.pkl')
-        self.movies = pd.read_csv('./cleaned_datasets/movies.dat', 
-                        delimiter='::', 
-                        encoding = 'latin1',
-                        header = None,
-                        engine = 'python',
-                        names = ['movie_id', 'title', 'genres']
-                        )
+        self.movies = pd.read_csv(
+            './datasets/movie_lens-1M/movies.dat', 
+            delimiter='::', 
+            encoding = 'latin1',
+            header = None,
+            engine = 'python',
+            names = ['movie_id', 'title', 'genres']
+        )
